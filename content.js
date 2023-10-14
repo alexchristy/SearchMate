241849d54cd46860203b047a6dca4f543754ebdb
Nareshp1 <44296460+Nareshp1@users.noreply.github.com>
2023-10-13 21:33:42 -0400

    No function content.js

--- content.js
+++ content.js
@@ -1,14 +1,14 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action === 'changeColor') {
-      document.body.style.backgroundColor = getRandomColor();
-    }
-  });
+// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+//     if (request.action === 'changeColor') {
+//       document.body.style.backgroundColor = getRandomColor();
+//     }
+//   });
   
-  function getRandomColor() {
-    var letters = '0123456789ABCDEF';
-    var color = '#';
-    for (var i = 0; i < 6; i++) {
-      color += letters[Math.floor(Math.random() * 16)];
-    }
-    return color;
-  }
+//   function getRandomColor() {
+//     var letters = '0123456789ABCDEF';
+//     var color = '#';
+//     for (var i = 0; i < 6; i++) {
+//       color += letters[Math.floor(Math.random() * 16)];
+//     }
+//     return color;
+//   }
