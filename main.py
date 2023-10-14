ba13c9e6731111703888a8c5cde98e62620d1e00
alexchristy <a.christy@ufl.edu>
2023-10-13 22:02:59 -0400

    Create skeleton of first endpoint

--- main.py
+++ main.py
@@ -6,73 +6,33 @@ logging.basicConfig(filename='app.log', level=logging.INFO, format='%(asctime)s 
 
 app = Flask(__name__)
 
-# Dummy data to act as a database
-tasks = [
-    {
-        'id': 1,
-        'title': 'Buy groceries',
-        'done': False
-    },
-    {
-        'id': 2,
-        'title': 'Go to the gym',
-        'done': True
-    }
-]
-
-# Get all tasks
-@app.route('/tasks', methods=['GET'])
-def get_tasks():
+@app.route('/find/page', methods=['POST'])
+def find_page():
     try:
-        return jsonify({'tasks': tasks}), 200
+        # Parse JSON data from the request
+        data = request.json
+
+        url = data.get('url', None)
+        query = data.get('query', None)
+
+        if url is None:
+            return jsonify({'error': 'Missing url parameter.'}), 400
+        
+        if query is None:
+            return jsonify({'error': 'Missing query parameter.'}), 400
+
+        # Log the received data
+        logging.info(f"Received url: {url}, query: {query}")
+
+        
+
+        return jsonify({'message': 'Data received'}), 200
+
     except Exception as e:
+        logging.error(f"An error occurred: {str(e)}")
         return jsonify({'error': str(e)}), 500
 
-# Get a single task by ID
-@app.route('/tasks/<int:task_id>', methods=['GET'])
-def get_task(task_id):
-    try:
-        task = next((item for item in tasks if item['id'] == task_id), None)
-        if task is None:
-            return jsonify({'error': 'Task not found'}), 404
-        return jsonify({'task': task}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
-# Create a new task
-@app.route('/tasks', methods=['POST'])
-def create_task():
-    try:
-        new_task = request.json
-        new_task['id'] = max(task['id'] for task in tasks) + 1 if tasks else 1
-        tasks.append(new_task)
-        return jsonify({'task': new_task}), 201
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-# Update an existing task by ID
-@app.route('/tasks/<int:task_id>', methods=['PUT'])
-def update_task(task_id):
-    try:
-        task = next((item for item in tasks if item['id'] == task_id), None)
-        if task is None:
-            return jsonify({'error': 'Task not found'}), 404
-        task.update(request.json)
-        return jsonify({'task': task}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
-
-# Delete a task by ID
-@app.route('/tasks/<int:task_id>', methods=['DELETE'])
-def delete_task(task_id):
-    try:
-        task = next((item for item in tasks if item['id'] == task_id), None)
-        if task is None:
-            return jsonify({'error': 'Task not found'}), 404
-        tasks.remove(task)
-        return jsonify({'message': 'Task deleted'}), 200
-    except Exception as e:
-        return jsonify({'error': str(e)}), 500
 
 if __name__ == '__main__':
     app.run(debug=True)
