d49f814467d75c29a7de23b79a908e067ecd4b61
Alex Christy <a.christy@ufl.edu>
2023-10-16 17:24:51 -0400

    Added headers to avoid web scraping detection

--- Backend/web_utils.py
+++ Backend/web_utils.py
@@ -4,6 +4,7 @@ from urllib.parse import urlparse
 import validators
 from bs4 import BeautifulSoup
 from requests.exceptions import RequestException
+import random
 
 # Configure logging
 logging.basicConfig(level=logging.INFO)
@@ -30,9 +31,59 @@ def fetch_page_content(url):
     Fetch the page content of a given URL and returns its text content for summary.
     Handles both paragraph-heavy and tag-specific pages like shopping sites.
     """
+
+    # List containing 3 sets of hyper-realistic headers
+    header_sets = [
+        {
+            "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/98.0.4758.102 Safari/537.36",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://www.google.com/",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Sec-Fetch-User": "?1",
+            "TE": "trailers"
+        },
+        {
+            "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/15.0 Safari/605.1.15",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Accept-Encoding": "gzip, deflate, br",
+            "Referer": "https://www.google.com/",
+            "Connection": "keep-alive",
+            "Upgrade-Insecure-Requests": "1",
+            "Sec-Fetch-Dest": "document",
+            "Sec-Fetch-Mode": "navigate",
+            "Sec-Fetch-Site": "cross-site",
+            "Sec-Fetch-User": "?1",
+            "TE": "trailers"
+        },
+        {
+        "User-Agent": "Mozilla/5.0 (X11; Ubuntu; Linux x86_64; rv:95.0) Gecko/20100101 Firefox/95.0",
+        "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+        "Accept-Language": "en-US,en;q=0.5",
+        "Accept-Encoding": "gzip, deflate, br",
+        "Referer": "https://www.google.com/",
+        "Connection": "keep-alive",
+        "Upgrade-Insecure-Requests": "1",
+        "Sec-Fetch-Dest": "document",
+        "Sec-Fetch-Mode": "navigate",
+        "Sec-Fetch-Site": "cross-site",
+        "Sec-Fetch-User": "?1",
+        "TE": "trailers"
+        }
+    ]
+
+    # How to use: Just pick a random set of headers from the list when making a request
+    headers = random.choice(header_sets)
+
     try:
         # Fetch the page content using requests
-        response = requests.get(url=url, timeout=3)
+        response = requests.get(url=url, timeout=3, headers=headers)
         
         # Raise an error if status code indicates failure
         response.raise_for_status()
